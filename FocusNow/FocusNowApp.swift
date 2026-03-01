c5d3a15491954cc061088353b00a8e2162af8269
leo_pe2 <l.penzenstadler@protonmail.com>
2026-03-01 15:05:27 +0100

    default

---
+++ FocusNow/FocusNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusNowApp.swift
+//  FocusNow
+//
+//  Created by Leonard Penzenstadler on 01.03.26.
+//
+
+import SwiftUI
+
+@main
+struct FocusNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
